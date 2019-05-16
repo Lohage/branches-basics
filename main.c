9bd78794491fc03534b585d12f2fecb90dbe8a09
Lohage <desmond999.2000@mail.ru>
2019-05-17 00:41:37 +0700

    add retry

--- main.c
+++ main.c
@@ -4,13 +4,20 @@
 
 int main()
 {
-	int i, j = 10;
-	printf("Enter ur number\n");
-	scanf("%d", &i);
-	if (i < j)
-		printf("Defeat\n");
-	else if (i > j)
-		printf("Victory!!\n");
-	else
-		printf("Draw\n");
+	int i, j, n = 1, k;
+	for (k = 0; k < n; k++){
+		j = 54
+		printf("Enter ur number 0 to 100\n");
+		scanf("%d", &i);
+		if (i < j)
+			printf("Defeat\n");
+		else if (i > j)
+			printf("Victory!!\n");
+		else
+			printf("Draw\n");
+		printf("Try again?\n1)Yes\n2)No\n");
+		scanf("%d", &j);
+		if(j == 1)
+			n++;
+	}
 }
